681741bd210647ebc4a9182f52447d6faabcb5b5
COEVO <grace@514.com.tw>
2016-02-19 19:17:11 +0800

    首頁版面調整完成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,78 +7,48 @@
 
     <title>514生活頻道 - 讓生活更有意思</title>
 
-    <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+    {{--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">--}}
 
-    <!-- Styles -->
+    <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css" media="screen"/>
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
+    <!--  right slidebar -->
+    <link href="css/slidebars.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+    
     <link href="css/project.css" rel="stylesheet">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
-    <style>
-        body {
-            font-family: 'Lato';
-        }
-
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
 </head>
 <body id="app-layout">
-    <nav class="home-navbar">
-        <div class="container">
-            <div class="navbar-header">
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="home-navbar-brand" href="{{ url('/') }}">
-                    <img src="/img/pics/logofin_white.png">
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right test">
-                    <!-- Authentication Links -->
-                    <li><a href="{{ url('blogs') }}">部落格</a></li>
-                    <li><a href="{{ url('') }}">辦活動</a></li>
-
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">登入</a></li>
-                        <li><a href="{{ url('/register') }}">註冊</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('profile') }}"><i class="fa fa-user-md"></i>個人資料</a></li>
-                                <li><a href="{{ url('follows') }}"><i class="fa fa-refresh fa-spin"></i>我的關注(0)</a></li>
-                                <li><a href="{{ url('friends') }}"><i class="fa fa-refresh fa-spin"></i>我的朋友(0)</a></li>
-                                <li><a href="{{ url('activitys') }}"><i class="fa fa-cog fa-spin"></i>我的活動</a></li>
-                                <li><a href="{{ url('logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
-
+    
+    @include('partials.header')
+    
     @yield('content')
     
+    @include('partials.footer')
+    
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="js/jquery.sparkline.js" type="text/javascript"></script>
+    <script src="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
+    <script src="js/owl.carousel.js" ></script>
+    <!--right slidebar-->
+    <!--common script for all pages-->
+<!--    <script src="js/common-scripts.js"></script>-->
+    <!--script for this page only-->
+    <script src="js/sparkline-chart.js"></script>
+    <script src="js/easy-pie-chart.js"></script>
 {{--     <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,66 @@
+
+<div class="footer-area">
+    <div class="bottom-linkout">
+        <img src="img/pics/igicon.png">
+        <img src="img/pics/fbicon.png">
+        <img src="img/pics/youtubeicon.png">
+    </div>
+    <!--
+    <div class="bottom_media">
+        <div class="media_title">媒體報導</div>       
+    </div>
+-->
+    <div class="footer-info">
+        <div class="col-md-12 col-lg-12">
+            <div class="footer-center">
+                <div class="how-conduct col-md-2 col-lg-2">
+                    <p>舉辦活動</p>
+                    <div class="how-conduct-content">
+                        <li>常見問題</li>
+                        <li>如何辦活動</li>
+                        <li>如何成為活動王</li>
+                        <div class="how-to-tips"><img src="img/pics/howtodo.png"></div>
+                    </div>
+                </div>
+                <div class="funtion-intro col-md-2 col-lg-2">
+                    <p>功能介紹</p>
+                    <div class="funtion-intro-content">
+                        <li>參加活動</li>
+                        <li>如何使用點數</li>
+                        <li>常見問題</li>
+                        <li>514部落格</li>
+                        <li>生活頻道</li>
+                    </div>
+                </div>
+                <div class="about-us col-md-2 col-lg-2">
+                    <p>關於我們</p>
+                    <div class="about-us-content">
+                        <li>關於514</li>
+                        <li>加入我們</li>
+                        <li>廣告合作</li>
+                        <li>媒體資源</li>
+                        <li>合作夥伴</li>
+                        <li>服務與隱私權條款</li>
+                    </div>
+                </div>
+                <div class="contact-us col-md-2 col-lg-2">
+                    <p>聯絡我們</p>
+                    <p class="contact-icon">
+                        <img src="img/pics/email.png">
+                        <span>mis@514.com.tw</span>
+                    </p>
+                    <p class="contact-icon">
+                        <img src="img/pics/phoneicon.png">
+                        <span>+886 2-2767-514-6</span>
+                    </p>
+                    <div class="day-to-day"><img src="img/pics/footer-time.png"></div>
+                </div>
+            </div>
+        </div>
+        <div class="online-secretary">
+            <p>線上客服小秘書</p>
+        </div>
+    </div>
+    <div class="copyright">Copyright © 2016 514 Inc. All rights reserved
+    </div>
+</div>
