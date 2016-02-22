939a409eab5b959fdd7c305af4b8fb9e4e09f9a6
COEVO <grace@514.com.tw>
2016-02-22 17:02:32 +0800

    更改blog logo

--- resources/views/blog/partials/header.blade.php
+++ resources/views/blog/partials/header.blade.php
@@ -1,46 +1,47 @@
-<nav class="other-navbar">
-        <div class="container">
-            <div class="navbar-header">
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+<nav class="navbar {{ Request::is('/') ? 'home-navbar' : 'other-navbar' }}">
+    <div class="container">
+        <div class="navbar-header">
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="glyphicon glyphicon-list"></span>
+                <span class="sr-only">選單</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
 
-                <!-- Branding Image -->
-                <a class="home-navbar-brand" href="{{ url('/') }}">
-                    <img src="/img/pics/514logobrown.png">
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav other-navbar-right">
-                    <!-- Authentication Links -->
-                    <li><a href="{{ url('blogs') }}">部落格</a></li>
-                    <li><a href="{{ url('') }}">辦活動</a></li>
-
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">登入</a></li>
-                        <li><a href="{{ url('/register') }}">註冊</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('profile') }}"><i class="fa fa-user-md"></i>個人資料</a></li>
-                                <li><a href="{{ url('follows') }}"><i class="fa fa-refresh fa-spin"></i>我的關注(0)</a></li>
-                                <li><a href="{{ url('friends') }}"><i class="fa fa-refresh fa-spin"></i>我的朋友(0)</a></li>
-                                <li><a href="{{ url('activitys') }}"><i class="fa fa-cog fa-spin"></i>我的活動</a></li>
-                                <li><a href="{{ url('logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
+            <!-- Branding Image -->
+            <a class="{{ Request::is('/') ? 'home-navbar-brand' : 'other-navbar-brand' }}" href="{{ url('/') }}">
+                <img src="/img/pics/logo_blog.png">
+            </a>
         </div>
-    </nav>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Right Side Of Navbar -->
+            <ul class="{{ Request::is('/') ? 'navbar-right' : 'other-navbar-right' }} nav navbar-nav">
+                <!-- Authentication Links -->
+                <li><a href="{{ url('blog') }}">部落格</a></li>
+                <li><a href="{{ url('') }}">辦活動</a></li>
+
+                @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">登入</a></li>
+                    <li><a href="{{ url('/register') }}">註冊</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true">
+                            gggg <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('profile') }}"><i class="fa fa-user-md"></i>個人資料</a></li>
+                            <li><a href="{{ url('follows') }}"><i class="fa fa-refresh fa-spin"></i>我的關注(0)</a></li>
+                            <li><a href="{{ url('friends') }}"><i class="fa fa-refresh fa-spin"></i>我的朋友(0)</a></li>
+                            <li><a href="{{ url('activitys') }}"><i class="fa fa-cog fa-spin"></i>我的活動</a></li>
+                            <li><a href="{{ url('logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
