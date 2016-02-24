9d484deee85630e0fee0b8d2bb2d52ea1e3c45c6
COEVO <grace@514.com.tw>
2016-02-24 10:52:12 +0800

    首頁寬度改為max-width

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,57 +1,53 @@
 
 <div class="footer-area">
-    <div class="bottom-linkout">
-        <img src="img/pics/igicon.png">
-        <img src="img/pics/fbicon.png">
-        <img src="img/pics/youtubeicon.png">
-    </div>
     <!--
     <div class="bottom_media">
         <div class="media_title">媒體報導</div>       
     </div>
 -->
+    <div class="bottom-linkout">
+        <img src="img/pics/igicon.png">
+        <img src="img/pics/fbicon.png">
+        <img src="img/pics/youtubeicon.png">
+    </div>
     <div class="footer-info">
         <div class="col-md-12 col-lg-12">
             <div class="footer-center">
-                <div class="how-conduct col-md-2 col-lg-2">
-                    <p>舉辦活動</p>
-                    <div class="how-conduct-content">
-                        <li>常見問題</li>
-                        <li>如何辦活動</li>
-                        <li>如何成為活動王</li>
-                        <div class="how-to-tips"><img src="img/pics/howtodo.png"></div>
-                    </div>
-                </div>
-                <div class="funtion-intro col-md-2 col-lg-2">
+                <div class="funtion-intro col-md-3 col-lg-3">
                     <p>功能介紹</p>
                     <div class="funtion-intro-content">
-                        <li>參加活動</li>
-                        <li>如何使用點數</li>
-                        <li>常見問題</li>
-                        <li>514部落格</li>
-                        <li>生活頻道</li>
+                        <a href="FAQ"><li>常見問題</li></a>
+                        <a href="HostGuide"><li>如何舉辦活動</li></a>
+                        <a href="PlayGuide"><li>如何報名活動</li></a>
+                        <a href="PointUse"><li>如何使用點數</li></a>
                     </div>
                 </div>
-                <div class="about-us col-md-2 col-lg-2">
+                <div class="about-us col-md-3 col-lg-3">
                     <p>關於我們</p>
                     <div class="about-us-content">
-                        <li>關於514</li>
-                        <li>加入我們</li>
-                        <li>廣告合作</li>
-                        <li>媒體資源</li>
+                        <a href="About"><li>關於 514</li></a>
+                        <a href="Join"><li>加入我們</li></a>
+                        <a href="Advertising"><li>廣告/合作專區</li></a>
+<!--
+                        <li>媒體報導</li>
                         <li>合作夥伴</li>
-                        <li>服務與隱私權條款</li>
+-->
+                        <a href="Privacy"><li>服務與隱私權條款</li></a>
                     </div>
                 </div>
                 <div class="contact-us col-md-2 col-lg-2">
                     <p>聯絡我們</p>
-                    <p class="contact-icon">
-                        <img src="img/pics/email.png">
-                        <span>mis@514.com.tw</span>
+                    <p class="contact-info">
+                        <a href="mailto:mis@514.com.tw">
+                            <img src="img/pics/email.png">
+                            <span>mis@514.com.tw</span>
+                        </a>
                     </p>
-                    <p class="contact-icon">
-                        <img src="img/pics/phoneicon.png">
-                        <span>+886 2-2767-514-6</span>
+                    <p class="contact-info">
+                        <a href="tel:+886 2-2767-514-6">
+                            <img src="img/pics/phoneicon.png">
+                            <span>+886 2-2767-514-6</span>
+                        </a>
                     </p>
                     <div class="day-to-day"><img src="img/pics/footer-time.png"></div>
                 </div>
@@ -61,6 +57,7 @@
             <p>線上客服小秘書</p>
         </div>
     </div>
+
     <div class="copyright">Copyright © 2016 514 Inc. All rights reserved
     </div>
 </div>
