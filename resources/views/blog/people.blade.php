cf5f678147945bdc69ea254f7eb0d4ad29bd11da
Grace <grace@514.com.tw>
2016-03-03 14:47:40 +0800

    新增部落格類別（例：人物）頁面

---
+++ resources/views/blog/people.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.blog') @section('content')
+<div class="blog-category-container">
+    <div class="row blog-category-content">
+        <div class="row blog-category-logo">
+            <p>人物</p>
+            <img src="/img/pics/somebody.png">
+        </div>
+        <div class="row blog-category-panel">
+            <a href="#">
+                <div class="blog-category-thumnail" 
+                     style="background-image:url('/img/pics/table-690892_1280.jpg')">
+                </div>
+            </a>
+            <div class="blog-category-text">
+                <div class="category-title">
+                    <a href="#">寧靜的午後一起喝下午茶，在迷惘的青春中找回獨一無二的自己</a>
+                </div>
+                <div class="category-info">
+                    <p><span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                        by 小班
+                    </p>
+                    <p><span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+                        2016-02-29 20:39
+                    </p>
+                </div>
+                <div class="category-description word-indent">我記得1991年，因為《牯嶺街少年殺人事件》，楊德昌帶著我一起去參加柏林影展，有一次餐聚，席間我記得還有李安，那時候楊德昌聊到他想拍的下一部電影，就是兩個長得一模一樣的人。只是當時因為大家都在喝酒、聊天，沒有特別探究細節。 過了一段時間，楊德昌又提起這個構想，甚至好像劇本已經寫了一半。劇中的人物，一個叫小五，一個叫阿成，打算請金城武來演。當然，現在劇中人物的選角和名字都已經不同，當初我們並沒有花太多時間討論探究劇本，但這個點子一直都種在我的心中。 為什麼呢？我認為電影是一個很寫實的媒介，反應當代現實生活。我想講的就是一個功利社會的題材。劇中我一樣安排了兩個長得一模一樣的人，這在現實中很難發生，但擺在電影中就帶有寓言的意義。
+                </div>
+                <div class="read-article">
+                    <a href="#">觀看文章 >></a>
+                </div>
+            </div>
+        </div>
+        <div class="row blog-category-panel">
+            <a href="#">
+                <div class="blog-category-thumnail" 
+                     style="background-image:url('/img/pics/table-690892_1280.jpg')">
+                </div>
+            </a>
+            <div class="blog-category-text">
+                <div class="category-title">
+                    <a href="#">小紅帽的野餐趴</a>
+                </div>
+                <div class="category-info">
+                    <p><span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                        by 小班
+                    </p>
+                    <p><span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+                        2016-02-29 20:39
+                    </p>
+                </div>
+                <div class="category-description  word-indent">我記得1991年，因為《牯嶺街少年殺人事件》，楊德昌帶著我一起去參加柏林影展，有一次餐聚，席間我記得還有李安，那時候楊德昌聊到他想拍的下一部電影，就是兩個長得一模一樣的人。只是當時因為大家都在喝酒、聊天，沒有特別探究細節。 過了一段時間，楊德昌又提起這個構想，甚至好像劇本已經寫了一半。劇中的人物，一個叫小五，一個叫阿成，打算請金城武來演。當然，現在劇中人物的選角和名字都已經不同，當初我們並沒有花太多時間討論探究劇本，但這個點子一直都種在我的心中。 為什麼呢？我認為電影是一個很寫實的媒介，反應當代現實生活。我想講的就是一個功利社會的題材。劇中我一樣安排了兩個長得一模一樣的人，這在現實中很難發生，但擺在電影中就帶有寓言的意義。
+                </div>
+                <div class="read-article">
+                    <a href="#">觀看文章 >></a>
+                </div>
+            </div>
+        </div>
+        <div class="row category-page-number">
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+        </div>
+    </div>
+
+</div>
+@endsection
