bbc07a44e29b5a5338f546938c6778f31c68de08
Lamon <lamon0624@gmail.com>
2016-03-04 14:10:27 +0800

    更新文章輸入驗證回應訊息

--- app/Http/Requests/BlogRequest.php
+++ app/Http/Requests/BlogRequest.php
@@ -25,7 +25,7 @@ class BlogRequest extends FormRequest
     public function rules()
     {
         return [
-            'title'       => 'required|unique:articles,title|min:3|max:255',
+            'title'       => 'required|unique:articles,title|min:3|max:255|regex:/^[(\x{4E00}-\x{9FA5})A-Za-z0-9\-,!@\.\(\)]+$/u',
             'created_at'  => 'required|date',
             'thumbnail'   => 'required|mimes:jpeg,bmp,png',
         ];
@@ -37,8 +37,9 @@ class BlogRequest extends FormRequest
             'title.required'      => '請務必填寫 文章標題 欄位',
             'title.min'           => '文章標題 最少 :min 字元',
             'title.max'           => '文章標題 最多 :max 字元',
+            'title.regex'         => '文章標題 請勿輸入空白或者特殊符號',
             'created_at.required' => '請務必填寫 發布時間 欄位',
-            'thumbnail.required'  => '請上傳一張圖片作為文章顯示圖',
+            'thumbnail.required'  => '請上傳一張圖片作為 文章顯示圖',
             'thumbnail.mimes'     => '縮圖的型態請為: :values.',
         ];
     }
