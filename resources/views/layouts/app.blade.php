75a5e487d112b8a9da579e1975b05d695228a12e
Lamon <lamon0624@gmail.com>
2016-03-07 20:16:25 +0800

    fix js error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,30 +10,30 @@
     <title>514生活頻道 - 讓生活更有意思</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/bootstrap-reset.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/bootstrap-reset.css')}}"/>
     <!--external css-->
-    <link href="{{asset('assets/font-awesome/css/font-awesome.css')}}" rel="stylesheet" />
-    <link href="{{asset('assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css')}}" rel="stylesheet" type="text/css')}}" media="screen"/>
-    <link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}" type="text/css')}}">
+    <link rel="stylesheet" href="{{asset('assets/font-awesome/css/font-awesome.css')}}"/>
+    <link rel="stylesheet" href="{{asset('assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css')}}" media="screen"/>
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}"/>
     <!--  right slidebar -->
-    <link href="{{asset('css/slidebars.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/slidebars.css')}}"/>
     <!-- Custom styles for this template -->
-    <link href="{{asset('css/style.css')}}" rel="stylesheet">
-    <link href="{{asset('css/style-responsive.css')}}" rel="stylesheet" />
-    <link href="{{asset('css/project.css')}}" rel="stylesheet">
-    <link href="{{asset('css/mobile.css')}}" rel="stylesheet" />
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="{{asset('css/style.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/style-responsive.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/project.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/mobile.css')}}"/>
+    {{-- <link rel="stylesheet" href="{{ elixir('css/app.css') }}"> --}}
 
 </head>
 <body id="app-layout">
 
-    {{--*/ 
+    {{--*/
         $header = (Request::is('/')) ? 'partials.header' : 'partials.page-header';
     /*--}}
 
     @include($header)
-    
+
     @yield('banner')
 
     @yield('content')
@@ -41,22 +41,22 @@
     @include('partials.footer')
 
     <!-- JavaScripts -->
-    <script src="{{asset('js/jquery.js')}}"></script>
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
-    <script type="text/javascript" src="js/jssor.slider.mini.js"></script>
-    <script src="{{asset('js/jquery.dcjqaccordion.2.7.js')}}"></script>
-    <script src="{{asset('js/jquery.scrollTo.min.js')}}"></script>
-    <script src="{{asset('js/jquery.nicescroll.js')}}" type="text/javascript"></script>
-    <script src="{{asset('js/jquery.sparkline.js')}}" type="text/javascript"></script>
-    <script src="{{asset('assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js')}}"></script>
-    <script src="{{asset('js/owl.carousel.js')}}" ></script>
+    <script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/jssor.slider.mini.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/jquery.dcjqaccordion.2.7.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/jquery.scrollTo.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/jquery.nicescroll.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/jquery.sparkline.js')}}"></script>
+    <script type="text/javascript" src="{{asset('assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/owl.carousel.js')}}"></script>
     <!--right slidebar-->
     <!--common script for all pages-->
-<!--    <script src="{{asset('js/common-scripts.js')}}"></script>-->
+<!--    <script type="text/javascript" src="{{asset('js/common-scripts.js')}}"></script>-->
     <!--script for this page only-->
-    <script src="{{asset('js/sparkline-chart.js')}}"></script>
-    <script src="{{asset('js/easy-pie-chart.js')}}"></script>
-{{--     <script src="{{ elixir('js/app.js') }}"></script> --}}
+    <script type="text/javascript" src="{{asset('js/sparkline-chart.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/easy-pie-chart.js')}}"></script>
+{{--     <script type="text/javascript" src="{{ elixir('js/app.js') }}"></script> --}}
 
     @yield('script')
 
