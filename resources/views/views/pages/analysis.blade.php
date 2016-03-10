61c9616ac2cf2649fd82b5518ca803190ecc97f0
Grace <grace@514.com.tw>
2016-03-10 14:19:31 +0800

    remove Sample view

--- resources/views/views/pages/analysis.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('app')
-@section('title') Analysis :: @parent @stop
-@section('content')
-    <div class="row">
-        <div class="page-header">
-            <h2>Analysis Page</h2>
-        </div>
-    </div>
-@endsection
