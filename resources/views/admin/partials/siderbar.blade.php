46dbd849c3cd7ac3e371d8b8f2e16a0d53d2ac9b
Lamon <lamon0624@gmail.com>
2016-03-23 11:58:36 +0800

    更新後台側欄選項

--- resources/views/admin/partials/siderbar.blade.php
+++ resources/views/admin/partials/siderbar.blade.php
@@ -24,11 +24,11 @@
                 <ul class="sub">
                     <li><a href="{{URL::to('dashboard/filter')}}">Filter 設定</a></li>
                     <li><a href="{{URL::to('dashboard/banner')}}">Banner 設定</a></li>
-                    <li><a href="{{URL::to('dashboard/ad')}}">廣告模組設定</a></li>
+                    <!-- <li><a href="{{URL::to('dashboard/ad')}}">廣告模組設定</a></li>
                     <li><a href="{{URL::to('dashboard/point')}}">特殊點數管理</a></li>
                     <li><a href="{{URL::to('dashboard/coupon')}}">優惠代碼管理</a></li>
                     <li><a href="{{URL::to('dashboard/invoice')}}">支出收入管理</a></li>
-                    <li><a href="{{URL::to('dashboard/analysis')}}">網站分析軟體</a></li>
+                    <li><a href="{{URL::to('dashboard/analysis')}}">網站分析軟體</a></li> -->
                 </ul>
             </li>
             <li class="sub-menu">
@@ -53,14 +53,14 @@
                         <li><a href="{{URL::to('dashboard/blog/create')}}">代為新增文章</a></li>
                         <li><a href="{{URL::to('dashboard/blog/category')}}">文章分類列表</a></li>
                         <li><a href="{{URL::to('dashboard/blog')}}">全部文章列表</a></li>
-                        <li><a href="{{URL::to('dashboard/blog/expert')}}">全部達人列表</a></li>
+                        <!-- <li><a href="{{URL::to('dashboard/blog/expert')}}">全部達人列表</a></li> -->
                     @endif
                     @if ( Auth::user()->adminer || Auth::user()->author )
                         <li><a href="{{URL::to('dashboard/blog/expert')}}">我要新增文章</a></li>
                         <li><a href="{{URL::to('dashboard/blog')}}">我的文章列表</a></li>
                     @endif
-                    <li><a href="{{URL::to('dashboard/blog/expert')}}">我喜愛的文章</a></li>
-                    <li><a href="{{URL::to('dashboard/blog/expert')}}">我關注的達人</a></li>
+                    <!-- <li><a href="{{URL::to('dashboard/blog/expert')}}">我喜愛的文章</a></li> -->
+                    <!-- <li><a href="{{URL::to('dashboard/blog/expert')}}">我關注的達人</a></li> -->
                     <li><a href="{{URL::to('dashboard/register/expert')}}">我要成為達人</a></li>
                 </ul>
             </li>
@@ -71,24 +71,29 @@
                 </a>
                 <ul class="sub">
                     @if ( Auth::user()->adminer )
+                        <!-- <li><a href="{{URL::to('dashboard/expert/checkout')}}">活動主查核</a></li> -->
                         <li><a href="{{URL::to('dashboard/activity/create')}}">代為新增活動</a></li>
                         <li><a href="{{URL::to('dashboard/activity/category')}}">活動分類列表</a></li>
                         <li><a href="{{URL::to('dashboard/activity')}}">全部活動列表</a></li>
+                        <li><a href="{{URL::to('dashboard/activity/check')}}">* 待審核活動</a></li>
                         <li><a href="{{URL::to('dashboard/activity/hoster')}}">活動廠商管理</a></li>
-                        <li><a href="{{URL::to('dashboard/activity/coupon')}}">優惠代碼管理</a></li>
+                        <!-- <li><a href="{{URL::to('dashboard/activity/coupon')}}">優惠代碼管理</a></li> -->
                     @endif
                     @if ( Auth::user()->adminer || Auth::user()->hoster )
                         <li><a href="{{URL::to('dashboard/activity/create')}}">我要新增活動</a></li>
-                        <li><a href="{{URL::to('dashboard/activity')}}">我主辦的活動</a></li>
-                        <li><a href="{{URL::to('dashboard/activity/invoice')}}">支出收入管理</a></li>
                     @endif
+                    @if ( Auth::user()->hoster )
                     <li><a href="{{URL::to('dashboard/activity')}}">我的活動列表</a></li>
-                    <li><a href="{{URL::to('dashboard/activity')}}">我過往的活動</a></li>
-                    <li><a href="{{URL::to('dashboard/activity')}}">我關注的活動</a></li>
-                    <li><a href="{{URL::to('dashboard/activity')}}">我要舉辦活動</a></li>
+                    <!-- <li><a href="{{URL::to('dashboard/my/activity/history')}}">過往辦的活動</a></li> -->
+                    <!-- <li><a href="{{URL::to('dashboard/activity/invoice')}}">支出收入管理</a></li> -->
+                    @else
+                    <li><a href="{{URL::to('dashboard/register/expert')}}">我要舉辦活動</a></li>
+                    @endif
+                    <li><a href="{{URL::to('dashboard/tickets')}}">我的活動票券</a></li>
+                    <!-- <li><a href="{{URL::to('dashboard/activity')}}">我關注的活動</a></li> -->
                 </ul>
             </li>
-            @if( Auth::user()->adminer )
+            @if ( Auth::user()->adminer)
             <li class="sub-menu">
                 <a href="javascript:;" >
                     <i class="fa fa-comments-o"></i>
