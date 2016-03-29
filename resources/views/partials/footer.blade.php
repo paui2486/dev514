522f84578e9bca388663795a1f5391ae084f710c
Grace <grace@514.com.tw>
2016-03-29 18:09:01 +0800

    FOOTER架構 css修改 類別title修改

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -13,7 +13,7 @@
     <div class="row  footer-info">
         
             <div class="row footer-center">
-                <div class="funtion-intro">
+                <div class="col-md-4 funtion-intro">
                     <p>功能介紹</p>
                     <div class="funtion-intro-content">
                         <a href="/blog"><li>514 部落格</li></a>
@@ -23,7 +23,7 @@
                         
                     </div>
                 </div>
-                <div class="about-us">
+                <div class="col-md-4 about-us">
                     <p>關於我們</p>
                     <div class="about-us-content">
                         <a href="/About"><li>關於 514</li></a>
@@ -33,27 +33,33 @@
                         <a href="/Privacy"><li>服務與隱私權條款</li></a>
                     </div>
                 </div>
-                <div class="contact-us">
+                <div class="col-md-4 col-xs-12 contact-us">
                     <p>聯絡我們</p>
                     <p class="contact-info">
-                        <a href="mailto:mis@514.com.tw">
-                            <img src="{{asset('img/pics/email.png')}}">
-                            <span>service@514.com.tw</span>
-                        </a>
+                        
+                            <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span><span><a href="mailto:mis@514.com.tw">
+                            service@514.com.tw</a></span>
+                        
                     </p>
                     <p class="contact-info">
-                        <a href="tel:+886 2-2767-514-6">
-                            <img src="{{asset('img/pics/phoneicon.png')}}">
-                            <span>+886 2-2767-514-6</span>
-                        </a>
+                        
+                            <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>
+                            <span><a href="tel:+886 2-2767-514-6">+886 2-2767-514-6</a></span>
+                        
                     </p>
-                    <div class="day-to-day">週一到週日10:00~18:00</div>
+                    <p class="order-time">
+                        <span class="glyphicon glyphicon-time" aria-hidden="true">
+                        </span>
+                        <span>週一到週日10:00~18:00</span>
+                    </p>
+                    <div class="online-secretary">
+                        <p><span class="glyphicon glyphicon-headphones" aria-hidden="true">
+                        </span>線上客服小秘書</p>
+                    </div>
                 </div>
             </div>
        
-        <div class="online-secretary">
-            <p>線上客服小秘書</p>
-        </div>
+        
     </div>
 
     <div class="copyright">Copyright © 2016 514 Inc. All rights reserved
