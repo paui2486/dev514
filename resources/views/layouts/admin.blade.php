a8ad211625042db943ca685041bcc3d20dab7e65
Lamon <lamon0624@gmail.com>
2016-05-05 16:28:33 +0800

    Add GA

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -41,6 +41,14 @@
                 version : 'v2.5',
             });
         };
+        
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-72845688-2', 'auto');
+        ga('send', 'pageview');
     </script>
     @yield('scripts')
     <script type="text/javascript" src="{{ asset('js/coevo-0.0.1.js') }}"></script>
