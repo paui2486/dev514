7b0201e82e03ca679c941e06eeae3b1f72daffe2
Lamon <lamon0624@gmail.com>
2016-05-13 15:44:02 +0800

    記錄FB登陸與查詢本地會員資訊 以及登入後前往該目的地等功能

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Session;
+
+use Log;
+use Input;
+use Redirect;
 use Socialite;
 use App\Http\Requests;
 use App\SocialAccountService;
@@ -17,11 +20,13 @@ class SocialAuthController extends Controller
         return Socialite::driver('facebook')->redirect();
     }
 
-    public function fbCallback()
+    public function fbCallback(Request $request)
     {
         $service = new SocialAccountService();
         $user = $service->createOrGetUser(Socialite::driver('facebook')->user());
+        Log::error(json_encode($user));
+
         auth()->login($user);
-        return redirect()->to('/');
+        return Redirect::intended();;
     }
 }
