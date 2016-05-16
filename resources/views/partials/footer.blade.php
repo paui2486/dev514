e8ce032fe721efcb22d4350fa6231c66debfb4e4
Grace <grace@514.com.tw>
2016-05-16 17:41:02 +0800

    更改footer排版

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,8 +5,7 @@
         <div class="media_title">媒體報導</div>
     </div>
 -->
-    <div class="row footer-topblock">
-        <div class="row footer-center">
+        
 <!--
             <div class="col-md-6 bottom-linkout">
                 <a href="https://www.instagram.com/"><img src="{{asset('img/pics/igicon.png')}}"></a>
@@ -14,19 +13,20 @@
                     <a href="https://www.youtube.com/?gl=TW&hl=zh-TW&noredirect=1"><img src="{{asset('img/pics/youtubeicon.png')}}"></a>
             </div>
 -->
-            <div class="book-us">
-                <p><img src="/img/icons/gifticon.png">514活動懶人包<img src="/img/icons/freeicon.png" style="width: 50px;margin-left: 5px;"></p>
-                <span>我們將提供給您最新的活動資訊，給您第一手有意思的活動內容！</span>
-                <div class="row" style="margin:10px 0;">
-                    <input class="col-md-9 col-xs-9" type="email" name="email" placeholder="輸入email，訂閱活動資訊">
-                    <div class="book-us-submit col-md-3 col-xs-3" id="subscribe">確定</div>
-                </div>
-            </div>
-        </div>
-    </div>
+<!--            <iframe class="col-md-6" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fiwinetodayshop%2F&tabs&width=300&height=200&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="300" height="150" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>-->
+            
+             
     <div class="row footer-info">
             <div class="row footer-center">
-                <div class="col-md-4 funtion-intro">
+                <div class="book-us col-md-5 col-xs-12">
+                    <p><img src="/img/icons/gifticon.png">514活動懶人包<img src="/img/icons/freeicon.png" style="width: 50px;margin-left: 5px;"></p>
+                    <span>馬上輸入E-mail，我們將提供給您最新的活動資訊，給您第一手有意思的活動內容！</span>
+                    <div class="row" style="margin:10px 0;">
+                        <input class="col-md-9 col-xs-9" type="email" name="email" placeholder="輸入email，訂閱活動資訊">
+                        <div class="book-us-submit col-md-3 col-xs-3" id="subscribe">確定</div>
+                    </div>
+                </div>
+                <div class="col-md-2 funtion-intro">
                     <p>功能介紹</p>
                     <div class="funtion-intro-content">
                         <a href="/"><li>514 部落格</li></a>
@@ -36,17 +36,16 @@
 
                     </div>
                 </div>
-                <div class="col-md-4 about-us">
+                <div class="col-md-2 about-us">
                     <p>關於我們</p>
                     <div class="about-us-content">
                         <a href="/About"><li>關於 514</li></a>
                         <a href="/Join"><li>加入我們</li></a>
-                        <a href="/Advertising"><li>廣告合作專區</li></a>
-                        <a href="/Cooperation"><li>企業合作專區</li></a>
+                        <a href="/Cooperation"><li>廣告/企業合作專區</li></a>
                         <a href="/Privacy"><li>服務與隱私權條款</li></a>
                     </div>
                 </div>
-                <div class="col-md-4 col-xs-12 contact-us">
+                <div class="col-md-3 col-xs-12 contact-us">
                     <p class="contact-p">聯絡我們</p>
                     <p class="contact-info">
                             <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span><span><a href="mailto:service@514.com.tw">
@@ -66,6 +65,7 @@
                         </span>線上客服小秘書</p>
                     </div>
                 </div>
+                
             </div>
     </div>
     <div class="gotop"><img src="/img/pics/backtotop.png"></div>
