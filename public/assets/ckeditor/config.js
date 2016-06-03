d333f8ef131a19ad8113178d6f8a5b255f244e08
Lamon <lamon0624@gmail.com>
2016-06-03 10:53:22 +0800

    修正新增會員的錯誤

--- public/assets/ckeditor/config.js
+++ public/assets/ckeditor/config.js
@@ -12,11 +12,11 @@ CKEDITOR.editorConfig = function( config ) {
 
   // config.filebrowserUploadUrl = '';
   // config.filebrowserBrowseUrl = '/assets/ckfinder/ckfinder.html';
-  // config.filebrowserImageBrowseUrl = '/assets/ckfinder/ckfinder.html?Type=Image';
+  // config.filebrowserImageBrowseUrl = '/assets/ckfinder/ckfinder.html?Type=Images';
   // config.filebrowserFlashBrowseUrl = '/assets/ckfinder/ckfinder.html?Type=Flash';
-  // config.filebrowserUploadUrl = '/assets/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
-  // config.filebrowserImageUploadUrl = '/assets/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Image';
-  // config.filebrowserFlashUploadUrl = '/assets/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
+  config.filebrowserUploadUrl = '/assets/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
+  config.filebrowserImageUploadUrl = '/assets/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
+  config.filebrowserFlashUploadUrl = '/assets/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
 
   // config.font_names = 'Arial;Arial Black;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana;新細明體;細明體;標楷體;微軟正黑體';
   // config.fontSize_sizes ='8/8px;9/9px;10/10px;11/11px;12/12px;13/13px;14/14px;15/15px;16/16px;17/17px;18/18px;19/19px;20/20px;21/21px;22/22px;23/23px;24/24px;25/25px;26/26px;28/28px;36/36px;48/48px;72/72px'
