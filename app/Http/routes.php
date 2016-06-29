7fb547eb902f91b4609bc4092a652d019bba6555
Lamon <lamon0624@gmail.com>
2016-06-29 17:18:41 +0800

    修正 route for search

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -58,7 +58,8 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('register/verify/{confirm}'  , 'MainController@confirm'            );
 
     Route::get('activity'                   , 'ActivityController@showResult'     );
-    Route::post('activity'                   , 'ActivityController@showResult'    );
+    Route::post('activity'                  , 'ActivityController@showResult'     );
+    Route::get('activity/data'              , 'ActivityController@showResult'     );
     Route::post('activity/data'             , 'ActivityController@showResult'     );
     Route::get('activity/{id}'              , 'ActivityController@index'          );
     // Route::get('activity/{category}'        , 'ActivityController@showCategory'  );
